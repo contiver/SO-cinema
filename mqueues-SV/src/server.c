2f56d74d850ea954b61477bb84928301936da946
Val <valserber@gmail.com>
2014-04-28 13:42:05 -0300

    queues sv arreglado

--- mqueues-SV/src/server.c
+++ mqueues-SV/src/server.c
@@ -14,10 +14,17 @@ main(void){
     RespMsg respMsg;
     signal(SIGINT, onSigInt);
 
+	FILE * file1=fopen("/tmp/mqueues-SV-clients","wb");
+	FILE * file2=fopen("/tmp/mqueues-SV-server","wb");
+	fclose(file1);
+	fclose(file2);
+
     if( (msqin = msgget(CLIENTS_KEY, IPC_CREAT | 0666)) == -1 )
         fatal("msgget");
     if ( (msqout = msgget(SERVER_KEY, IPC_CREAT | 0666)) == -1 )
         fatal("msgget");
+
+	printf("%d  %d\n",msqin,msqout);
 
     for(;;){
         if( msgrcv(msqin, (char *)&reqMsg, sizeof(reqMsg), 0, 0) == -1 ){
@@ -28,6 +35,7 @@ main(void){
         }
         respMsg.resp = execRequest(reqMsg.req);
         respMsg.mtype = reqMsg.mtype;
+	sleep(10);
         if( msgsnd(msqout, (char *)&respMsg, sizeof(RespMsg), 0) == -1 ){
 			perror("msgsnd");
 			break;
@@ -42,6 +50,8 @@ terminateServer(void){
     int exit_status = EXIT_SUCCESS;
     if( msqin != -1 && msgctl(msqin, IPC_RMID, NULL) == -1 ) exit_status = EXIT_FAILURE;
     if( msqout != -1 && msgctl(msqout, IPC_RMID, NULL) == -1 ) exit_status = EXIT_FAILURE;
+ 	if( remove("/tmp/mqueues-SV-clients") == -1 ) exit_status = EXIT_FAILURE;  
+	if( remove("/tmp/mqueues-SV-server") == -1 ) exit_status = EXIT_FAILURE;  
     exit(exit_status);
 }
 
